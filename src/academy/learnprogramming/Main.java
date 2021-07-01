f7f19e88eb067b2e8906ee1f144f1f5289710d1f
Romano Livi <romano_livi@outlook.com>
2021-07-01 18:52:23 -0400

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String varFour = "This is private to main";
+
+	    ScopeCheck scopeInstance = new ScopeCheck();
+	    scopeInstance.useInner();
+
+	    ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+
+	    // can only access innerClass.varThree if it is set to public.
+        System.out.println("varThree is not accessible here " + innerClass.varThree);
+	    //
+//        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+//        System.out.println(varFour);
+//
+//        scopeInstance.timesTwo();
+//
+//        System.out.println("===========");
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
+//
+
+    }
+}
